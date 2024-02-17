07c2b44f10406652812486e34706827e1fa8e598
UniversalTaco <160229211+UniversalTaco@users.noreply.github.com>
2024-02-17 11:12:33 +0800

    Update magazine.js

--- magazine.js
+++ magazine.js
@@ -5,43 +5,36 @@ let firstEntry;
 
 function setup() {
     // Create canvas
-    createCanvas(windowWidth - 50, windowHeight - 50);
+    createCanvas(windowWidth-20, windowHeight-20);
+    background("rgb(0,0,51)")
 
     // Get the first entry from local storage
     firstEntry = localStorage.getItem("firstEntry");
 
     // Check if it's the first entry
-    if(firstEntry === null || firstEntry === undefined) {
+    if(firstEntry === null) {
         localStorage.setItem("firstEntry", true);
+        firstEntry = localStorage.getItem("firstEntry");
         alert("This website is optimised for computers, but is meant to also be compatible with other devices as well!");
     }
 
-    // Set text size
-    textSize(50);
+    // Set text theming
+    textSize(windowWidth/37.5);
+    fill("yellow")
+    stroke("orange")
+    strokeWeight(4)
+
 
     // Display title
-    title = text("Access The CBSE 8C Class Magazine!", windowWidth / 2 - 420, windowHeight / 2 + 150);
+    title = text("Download The CBSE 8C Class Magazine!", windowWidth / 2 - 420, windowHeight / 2 + 150);
 
     // Create download button
-    downloadButton = createButton("Download (.DOCX)");
-    downloadButton.position(windowWidth / 2 - 75, windowHeight / 2 + 200);
-    downloadButton.mousePressed(downloadFile);
+    downloadButton = createA("https://raw.githubusercontent.com/UniversalTaco/CBSE_8C_Magazine/main/Document.docx", "Download (.DOCX)", "_blank");
+    downloadButton.position(windowWidth / 2 - 50, windowHeight / 2 + 200);
+    downloadButton.style('color', '#FFAA00')
 }
 
 function draw() {    
     // Log success message
     console.log("Website startup is a success!");
 } 
-
-function downloadFile() {
-    // Alert the user
-    alert("Redirecting you to another site!");
-
-    // File URL
-    let filePath = "https://raw.githubusercontent.com/UniversalTaco/CBSE_8C_Magazine/main/Document.docx";
-
-    // Open the file in a new tab
-    window.open(filePath, '_blank');
-}
-
-
